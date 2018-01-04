db21302eedb74c111217caacca68cff4e0c06b05
Momen Ali <engmomenali@gmail.com>
2018-01-04 20:36:54 +0200

    commit

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeAsynTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeAsynTaskTest.java
@@ -2,16 +2,18 @@ package com.udacity.gradle.builditbigger;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 import android.text.TextUtils;
 import android.util.Pair;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by Momen Ali on 1/4/2018.
  */
-
+@RunWith(AndroidJUnit4.class)
 public class JokeAsynTaskTest {
     String backendResult;
     Context instrumentationCtx;
@@ -20,7 +22,7 @@ public class JokeAsynTaskTest {
     *  I found this subject in Stack Overflow site */
     @Before
     public void setup() {
-
+        instrumentationCtx = InstrumentationRegistry.getContext();
     }
 
     @Test
@@ -31,7 +33,6 @@ public class JokeAsynTaskTest {
                 backendResult = result;
             }
         };
-        instrumentationCtx = InstrumentationRegistry.getContext();
         endpointsAsyncTask.execute(new Pair<Context, String>(instrumentationCtx, "Momen"));
 
         Thread.sleep(2000);
